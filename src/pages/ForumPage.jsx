87d27518a1041811dba50927b8fe1d979536f6da
Ivan. <92353523+IvanDaR1td@users.noreply.github.com>
2025-06-06 19:59:05 +0100

    Update ForumPage.jsx

--- src/pages/ForumPage.jsx
+++ src/pages/ForumPage.jsx
@@ -57,7 +57,6 @@ const ForumPage = () => {
       </div>
       <br /><br /><br />
       <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3PJnL0qaMd3C71Am5X8wjM?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      ></iframe>
     </>
   );
 };
